bb9717f77b347d75ae3a2d56af5f5c852ee197ef
log_e <log_e@qq.com>
2020-06-19 16:05:01 +0800

    update

--- src/test_string.cpp
+++ src/test_string.cpp
@@ -8,7 +8,7 @@ int test_string(const char *src, size_t len)
     if(len > 6){
         if (src[0] == 'v') {
             if (src[1] == 'b') {
-                memcpy((void*)(src + 444), src, len);
+                // memcpy((void*)(src + 444), src, len);
             }
         }
     }
