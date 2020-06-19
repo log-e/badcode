611a208cecc09b1ec22069446d6445094e6c63ec
log_e <log_e@qq.com>
2020-06-19 15:51:40 +0800

    init commit

---
+++ fuzz/fuzz_test_string.cpp
@@ -0,0 +1,11 @@
+#include "test_string.h"
+
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t * data, size_t size) {
+    test_string((const char *)data, size);
+    return 0;
+}
---
+++ src/test_string.cpp
@@ -0,0 +1,15 @@
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int test_string(const char *src, size_t len)
+{
+    if(len > 6){
+        if (src[0] == 'v') {
+            if (src[1] == 'b') {
+                memcpy((void*)(src + 444), src, len);
+            }
+        }
+    }
+}
